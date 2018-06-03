6061804c18fdc86979a943295fa08814560a974b
Lucas-Almada <32305770+Lucas-Almada@users.noreply.github.com>
2018-06-03 02:15:25 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,15 +4,17 @@ var json1 = "https://rawgit.com/Lucas-Almada/Noticias/master/json/n1.json";
 var json2 = "https://rawgit.com/Lucas-Almada/Noticias/master/json/n2.json";
 var listaJson = [json1, json2];
 var scrollactivate = true;
+var json = 0;
+var datos;
 
 $(document).ready(function () {
 	setInterval(cambiarImagen, 1500);
 
 	$("#cargmas").click(function () {
-		cargarNoticias();
+		cargarnoticia();
 	});
 
-	$("#botonscroll").click(function () {
+	$("#desactivate").click(function () {
 		if (scrollactivate) {
 			scrollactivate = false;
 		} else {
@@ -23,7 +25,7 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		if (scrollactivate) {
 			if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				cargarNoticias();
+				cargarnoticia();
 			}
 		}
 	});
@@ -45,14 +47,14 @@ function cargarnoticia() {
 
 	} else {
 		if (cargado) {
-			$getJSON(listaJson[1], function (jsonObject) {
-				datos = "<div class='row'> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[0].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[0].imgurl + "'/> </div> <p>" + jsonObject.noticias[0].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[1].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[1].imgurl + "'/> </div> <p>" + jsonObject.noticias[1].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[2].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[2].imgurl + "'/> </div> <p>" + jsonObject.noticias[2].content + "</p> </div> </div> </div>";
+			$.getJSON(listaJson[1], function(jsonObject){
+				datos = "<div class='row'> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[0].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[0].imgurl + "'/> </div> <p>" + jsonObject.noticia[0].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[1].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[1].imgurl + "'/> </div> <p>" + jsonObject.noticia[1].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[2].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[2].imgurl + "'/> </div> <p>" + jsonObject.noticia[2].content + "</p> </div> </div> </div>";
 				$("#contenedornoticias").append(datos);
 			});
 		} else {
 
-			$.getJSON(listaJson[0], function (jsonObject) {
-				datos = "<div class='row'> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[0].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[0].imgurl + "'/> </div> <p>" + jsonObject.noticias[0].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[1].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[1].imgurl + "'/> </div> <p>" + jsonObject.noticias[1].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticias[2].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticias[2].imgurl + "'/> </div> <p>" + jsonObject.noticias[2].content + "</p> </div> </div> </div>";
+			$.getJSON(listaJson[0], function(jsonObject){
+				datos = "<div class='row'> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[0].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[0].imgurl + "'/> </div> <p>" + jsonObject.noticia[0].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[1].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[1].imgurl + "'/> </div> <p>" + jsonObject.noticia[1].content + "</p> </div> </div> <div class='col-sm-4'> <div class='noticia'> <h1> " + jsonObject.noticia[2].title + "</h1> <div class='text-center'> <img class='imagenNoticia1' src='" + jsonObject.noticia[2].imgurl + "'/> </div> <p>" + jsonObject.noticia[2].content + "</p> </div> </div> </div>";
 				$("#contenedornoticias").append(datos);
 				cargado = true;
 				json++;
